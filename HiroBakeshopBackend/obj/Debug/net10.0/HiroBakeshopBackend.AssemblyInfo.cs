c45c09a102fa00d731babe51591bfc076d23827d
joseninogarcia06 <jngarcia08081988@gmail.com>
2026-03-10 14:47:35 +0800

    Add updates

--- HiroBakeshopBackend/obj/Debug/net10.0/HiroBakeshopBackend.AssemblyInfo.cs
+++ HiroBakeshopBackend/obj/Debug/net10.0/HiroBakeshopBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HiroBakeshopBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c23c11b4c1b9a823b49134bb02bc2aa188c3441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc5831b43c57e2dfe582746cb935116df0b7d2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HiroBakeshopBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HiroBakeshopBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

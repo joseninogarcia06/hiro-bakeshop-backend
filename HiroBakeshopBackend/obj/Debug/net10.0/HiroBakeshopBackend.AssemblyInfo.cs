1c6142f31baf289fb6c2e115796562a8db5a3cd7
joseninogarcia06 <jngarcia08081988@gmail.com>
2026-03-10 19:59:07 +0800

    Update dataseed 2

--- HiroBakeshopBackend/obj/Debug/net10.0/HiroBakeshopBackend.AssemblyInfo.cs
+++ HiroBakeshopBackend/obj/Debug/net10.0/HiroBakeshopBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HiroBakeshopBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207194c4939e12b78676692454d98822c16cc2b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ffc865e64e2168338ddeb1d931731c1f63dc8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HiroBakeshopBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HiroBakeshopBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
